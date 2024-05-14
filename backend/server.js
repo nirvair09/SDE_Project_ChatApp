f433c609eef854d5e23c66c27ce22a61709f38f8
nirvair09 <chandradeep262003@gmail.com>
2024-05-14 20:03:57 +0530

    removed guest login

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-// const path = require("path");
+const path = require("path");
 
 dotenv.config();
 connectDB();
